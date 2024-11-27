c75a7190b1338951364dda3612f2243408173f5f
unknown <mohamed.mustafa.soliman.31@gmail.com>
2024-11-27 18:49:44 +0200

    add

---
+++ myproject/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+# Create your views here.
